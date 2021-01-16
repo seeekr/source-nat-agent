13facc1ae6b91c7555cae93708651e7dd3369414
Denis Andrejew <da.colonel@gmail.com>
2021-01-16 21:02:47 +0100

    update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # using chokidar from npm as convenient file watcher
-chokidar '**/*.yaml' '**/*.go' -c "./build.sh && docker build . -f dev.Dockerfile -t source-nat-agent && docker tag source-nat-agent localhost:55001/source-nat-agent && docker push localhost:55001/source-nat-agent && kubectl apply -f agent.yaml && kubectl -n source-nat-agent delete po --all && echo done"
+chokidar '**/*.yaml' '**/*.go' -c "./build.sh && docker build . -f dev.Dockerfile -t source-nat-agent && docker tag source-nat-agent localhost:55001/source-nat-agent && docker push localhost:55001/source-nat-agent && kubectl apply -f agent.yaml && kubectl -n source-nat-agent delete po -l k8s-app=source-nat-agent && echo done"
